33c454475badcd8ebc53f8462c576ead24221773
benbasty <benbasty@yahoo.fr>
2022-07-17 10:32:03 +0800

    Working With Data Types 1

---
+++ src/com/projects/WorkingWithDataTypesFundationsTest.java
@@ -0,0 +1,9 @@
+package com.projects;
+
+public class WorkingWithDataTypesFundationsTest {
+    public static void main(String[] args) {
+        int a = Integer.MIN_VALUE;
+        int b = -a;
+        System.out.println(a + " " + b);
+    }
+}
